44186514533465d215737684c4113eec4629b179
seungbin.kim <seungbin.kim.dev@gmail.com>
2023-04-23 23:12:10 +0900

    프로젝트 생성

---
+++ src/main/java/much/api/MuchApiApplication.java
@@ -0,0 +1,13 @@
+package much.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/much/api/MuchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package much.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
